3c7948d05dec4c1ab5145c4db1fe9a9729266e50
dlgusrjs <dlgusrjs07@naver.com>
2024-11-21 22:56:37 +0900

    feat : 자유, Q&A게시판 글쓰기 DB연동 CSS, HTML 수정했슴다

--- src/main/java/curevengers/petit_cure/Dao/UserMapper.java
+++ src/main/java/curevengers/petit_cure/Dao/UserMapper.java
@@ -1,5 +1,6 @@
 package curevengers.petit_cure.Dao;
 
+import curevengers.petit_cure.Dto.QABoardDTO;
 import curevengers.petit_cure.Dto.freeBoardDTO;
 import curevengers.petit_cure.Dto.testDto;
 import org.apache.ibatis.annotations.Insert;
@@ -16,6 +17,12 @@ public interface UserMapper {
 //    @Insert("Insert Into 연결test values (#{id})") void insert(String id);
     public void insert(testDto dto);
     List<freeBoardDTO> findAllBoards();
+    List<QABoardDTO> findQAAllBoards();
 
     freeBoardDTO selectOne(String no);
+    QABoardDTO selectQAOne(String no);
+
+    void insertFreeBoard(freeBoardDTO dto);
+    void insertQABoard(QABoardDTO dto);
+
 }
--- src/main/java/curevengers/petit_cure/Service/testServiceImpl.java
+++ src/main/java/curevengers/petit_cure/Service/testServiceImpl.java
@@ -1,6 +1,7 @@
 package curevengers.petit_cure.Service;
 
 import curevengers.petit_cure.Dao.UserMapper;
+import curevengers.petit_cure.Dto.QABoardDTO;
 import curevengers.petit_cure.Dto.freeBoardDTO;
 import curevengers.petit_cure.Dto.testDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +28,39 @@ public class testServiceImpl implements testService {
         } catch (Exception e) {
             // 예외 로그 출력
             e.printStackTrace();
-            throw new RuntimeException("게시판 조회에 실패했습니다.");
+            throw new RuntimeException("자유게시판 조회에 실패했습니다.");
+        }
+    }
+
+    @Override
+    public List<QABoardDTO> getAllQABoards() {
+        try {
+            return userMapper.findQAAllBoards();
+        } catch (Exception e) {
+            // 예외 로그 출력
+            e.printStackTrace();
+            throw new RuntimeException("QA게시판 조회에 실패했습니다.");
         }
     }
 
     @Override
     public freeBoardDTO getBoardNo(String no) {
         return userMapper.selectOne(no);
+    }
+
+    @Override
+    public QABoardDTO getQABoardNo(String no) {
+        return userMapper.selectQAOne(no);
+    }
+
+    @Override
+    public void addFreeBoard(freeBoardDTO dto) {
+        userMapper.insertFreeBoard(dto);
+    }
+
+    @Override
+    public void addQABoard(QABoardDTO dto) {
+        userMapper.insertQABoard(dto);
     }
 
 
