b1eec2e1e7f7f6f73306c923f136dadd5323ba03
Younjung58 <skykh999@naver.com>
2024-11-24 23:30:53 +0900

    feat:회원가입 이메일인증 이메일인증 코드 생성 및 전송기능 추가 인증코드 검증기능은 보완필요

--- src/main/java/curevengers/petit_cure/Service/healthCheckService.java
+++
@@ -1,9 +0,0 @@
-package curevengers.petit_cure.Service;
-
-import curevengers.petit_cure.Dto.healthCheckDTO;
-import org.springframework.stereotype.Service;
-
-
-public interface healthCheckService {
-    public healthCheckDTO healthCheckResult(healthCheckDTO dto) throws Exception;
-}
