0e254f7f778f9845a542077ccab06f6bf168533d
Younjung58 <skykh999@naver.com>
2024-12-09 23:10:03 +0900

    feat:추천, 신고 추가 구현 우울증게시판만 이동은 구현중

---
+++ src/main/java/curevengers/petit_cure/Dto/alertDTO.java
@@ -0,0 +1,17 @@
+package curevengers.petit_cure.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class alertDTO {
+    int alertno;
+    int no;
+    String id;
+    String comment;
+    String date;
+    String alert_cate;
+}
